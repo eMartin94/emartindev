8501dce568c875b09daf805736d1134d81b230ab
Martin <eduardo.martin688@gmail.com>
2025-06-14 11:30:56 -0500

    updated metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,23 @@ export const metadata = {
   icons: {
     icon: '/favicon.ico',
   },
+  openGraph: {
+    title: 'eMartinDev | Portafolio',
+    description:
+      '"Desarrollador web enfocado en crear experiencias interactivas con React, Next.js y Tailwind CSS. Especialista en animaciones con GSAP y Framer Motion, y con experiencia en APIs, bases de datos y backend con Node.js.',
+    url: 'https://emartindev.vercel.app/',
+    siteName: 'eMartinDev | Portafolio',
+    images: ['/preview-1200x630'],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'eMartinDev | Portafolio',
+    description:
+      '"Desarrollador web enfocado en crear experiencias interactivas con React, Next.js y Tailwind CSS. Especialista en animaciones con GSAP y Framer Motion, y con experiencia en APIs, bases de datos y backend con Node.js.',
+    creator: '@emartindev',
+    images: ['/preview-1200x600'],
+  },
 };
 
 export default function RootLayout({ children }) {
